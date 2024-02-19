dfe4c2b03b83921e11c1d4b42cc1cfcab51e6351
Sarang Kharche <sarangkharche@gmail.com>
2024-02-19 12:50:26 +0000

    Changed landing page text

--- src/resources/default-md.js
+++ src/resources/default-md.js
@@ -1,10 +1,9 @@
-export const defaultMarkdownText = `
-# Sarang Codes Markdown Editor
+export const defaultMarkdownText = `# Sarang Codes Markdown Editor
 
 ## Mark Features 
 - Minimal and Quick
 - Supports commonmark standards
-- built to be to the point and add no overhead for people who simple enjoy writing in markdown 
+- Built to be to the point and add no overhead for people who simple enjoy writing in markdown
 
 ## Motivation 
 A simple markdown editor that is quick and easy to use.
