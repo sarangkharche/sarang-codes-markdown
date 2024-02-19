f559e1f3da89f952045f6cde56a575123ea6ebd1
Sarang Kharche <sarangkharche@gmail.com>
2024-02-19 13:57:21 +0000

    Added Vercel Analytics

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { inject } from '@vercel/analytics';
+
+inject();
 
 // https://vitejs.dev/config/
 export default defineConfig({
