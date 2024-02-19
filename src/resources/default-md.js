71b080fb70a63a29b0125d39c76881669a935304
Sarang Kharche <sarangkharche@gmail.com>
2024-02-19 13:00:24 +0000

    Changed landing page text

--- src/resources/default-md.js
+++ src/resources/default-md.js
@@ -6,6 +6,8 @@ export const defaultMarkdownText = `# Sarang Codes Markdown Editor
 - Minimalistic and quick
 - Responsive
 - Easy to use
+- Markdown syntax highlighting
+
 
 ## Motivation 
 A simple markdown editor that is quick and easy to use.
